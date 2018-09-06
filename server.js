d6d364ed2c2bb03697a184e49bee8f1164758ae2
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-09-06 17:49:22 +1200

    configuring history mode

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const history = require('connect-history-api-fallback')
 const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
 
 const app = express()
+app.use(history())
 
 app.use(serveStatic(path.join(__dirname + "/dist")))
 const port = process.env.PORT || 5000
