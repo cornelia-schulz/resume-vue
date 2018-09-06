be27c149071984e1980c2f3d2c9db3936a68d936
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-09-06 15:48:47 +1200

    fixed header responsiveness

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,10 +8,10 @@
                 <b-row class='header-text-right'>
                     <h1>Cornelia Schulz <span class='header-title'>Developer</span></h1>
                 </b-row>
-                <b-row>
-                    <b-col>Email: <a href="mailto:firzhugh@gmail.com">firzhugh@gmail.com</a></b-col>
-                    <b-col>Github: <a href='https://github.com/cornelia-schulz' target='_blank'>cornelia-schulz</a></b-col>
-                    <b-col>Location: Auckland</b-col>
+                <b-row id='info-row'>
+                    <b-col class='info'>Email: <a href="mailto:firzhugh@gmail.com">firzhugh@gmail.com</a></b-col>
+                    <b-col class='info'>Github: <a href='https://github.com/cornelia-schulz' target='_blank'>cornelia-schulz</a></b-col>
+                    <b-col class='info'>Location: Auckland</b-col>
                 </b-row>
             </b-col>
         </b-row>
@@ -23,51 +23,75 @@
 
 
 <script>
-    export default {
-        methods: {
-            openSidebar() {
-            document.getElementById("menu").style.display = "block"
+export default {
+  methods: {
+    openSidebar() {
+      document.getElementById('menu').style.display = 'block';
     },
-        closeSidebar() {
-            document.getElementById('menu').style.display = "none"
-    }
-        }    
-    }
+    closeSidebar() {
+      document.getElementById('menu').style.display = 'none';
+    },
+  },
+};
 </script>
 
 <style>
-    header {
-        padding-top: 20px;
-        background-color: #1D2D44;
-        padding-bottom: 20px;
-        color: #F0EBD8;
-    }
-    h1 {
-        text-align: left;
-        margin-left: 12px;
-        color: 748CAB !important;
-    }
+header {
+  padding-top: 20px;
+  background-color: #1d2d44;
+  padding-bottom: 20px;
+  color: #f0ebd8;
+}
+h1 {
+  text-align: left;
+  margin-left: 12px;
+  color: 748cab !important;
+}
 
-    .header-title {
-        font-size: 1rem;
-        color: #F0EBD8;
-    }
+.header-title {
+  font-size: 1rem;
+  color: #f0ebd8;
+}
 
-    .img {
-        text-align: center;
-    }
+.img {
+  text-align: center;
+  width: 100%;
+}
 
-    .profile-img {
-        text-align: center;
-        /* -webkit-box-shadow: 18px 18px 16px -15px rgba(116,140,171,1);
-        -moz-box-shadow: 18px 18px 16px -15px rgba(116,140,171,1);
-        box-shadow: 18px 18px 16px -15px rgba(116,140,171,1); */
-        max-height: 150px;
-        border-radius: 50%;
-    }
+.profile-img {
+  text-align: center;
+  max-height: 150px;
+  border-radius: 50%;
+}
 
-    .header-right {
-        margin-top: 70px;
-    }
+.header-right {
+  margin-top: 70px;
+}
 
+@media only screen and (max-width: 920px) {
+  .img {
+    display: none;
+  }
+  .header-right {
+    margin-top: 0;
+    padding-left: 50px;
+  }
+}
+
+@media only screen and (max-width: 400px) {
+  .img {
+    display: none;
+  }
+  .info {
+    display: block;
+    width: 100%;
+  }
+  .header-right {
+    margin-top: 0;
+    padding-left: 30px;
+  }
+  #info-row {
+    display: block;
+  }
+}
 </style>
